2bd196559eb80075df89b1e661439d3660796b4f
Md Sadman Sakib <sakib.csedu21@gmail.com>
2019-09-25 22:17:00 +0600

    Update hash_single.cpp

--- Batch 2/Class 13/hash_single.cpp
+++ Batch 2/Class 13/hash_single.cpp
@@ -15,17 +15,26 @@ void pre()
 {
 	power[0] = 1;
 	for(int i=1;i<=MAX;i++) power[i] = (power[i-1]*pr)%MOD;
-	Hash[0] = str[0]-'a';
+	Hash[0] = str[0]-'a'+1;
 	for(int i=1;i<len;i++)
 	{
-		Hash[i] = (Hash[i-1]*pr + str[i]-'a')%MOD;
+		Hash[i] = (Hash[i-1]*pr + str[i]-'a'+1)%MOD;
 	}
 	return;
 }
 
 ll getHash(int i,int j)
 {
-    return (Hash[j]-(Hash[i-1]*power[j-i+1])+MOD)%MOD;
+    if(!i) return Hash[j]%MOD;
+    return (Hash[j]-(Hash[i-1]*power[j-i+1])+MOD+MOD+MOD+MOD+MOD)%MOD;
+}
+
+ll generateHash(string &s)
+{
+    ll hashValue=0;
+    for(int i=0; i<(int)s.size(); i++)
+	    hashValue=((hashValue*BASE)%MOD+(s[i]-'a'+1))%MOD;
+    return hashValue%MOD;
 }
 
 int main()
@@ -33,7 +42,6 @@ int main()
 	cin >> str;
 	len = str.size();
 	pre();
-
-    cout << getHash(0,0) << " "<<getHash(1,1);
+    	cout << getHash(0,0) << " "<<getHash(1,1);
 	return 0;
 }
