aea19e619efcb60e9346d9db90dbfbf84af829b3
Md Sadman Sakib <sakib.csedu21@gmail.com>
2021-01-02 00:27:21 +0600

    new files

---
+++ DevSkill_CP/Beginner/Batch_4/Class_02/code.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /**
+        char - can store any character. Must be a single character. (1B = 8b)
+        int  -  can store any integer number within a specific range. (4B = 32b)
+        short int - can store any integer number within a specific range. (2B = 16b)
+        long long - can store any integer number within a specific range. (8B = 64b)
+
+        unsigned int - [0,2^32-1]
+        unsigned [int, short int, long long]
+
+        float - can store any floating value within a specific range. (4B = 32b)
+        double - can store any floating value within a specific range. (8B = 64b)
+    **/
+    short int sa = 52;
+    cout << sa;
+    return 0;
+}
