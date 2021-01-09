bf8e050092091c1b1502c448b15a04ef70ec5446
Md Sadman Sakib <sakib.csedu21@gmail.com>
2021-01-10 01:25:25 +0600

    new files

---
+++ DevSkill_CP/Beginner/Batch_4/Class_04/code.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int doIncrement(int c)
+{
+    return c + 1;
+}
+
+int main()
+{
+    /// for loop
+    /// while
+
+    /// for(initialization; condition; change of state)
+    /// some problems related to nested loop
+    for(int i = 0; i < 10; i++) /// 0,1,2,3,4,5,6,7,8,9,10
+    {
+        //cout << i << "\n";
+    }
+
+    /// while(condition)
+    int T;
+    scanf("%d", &T);
+    while(T--)
+    {
+        cout << T << "\n";
+    }
+
+    return 0;
+}
