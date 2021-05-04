809f7b29a17269e3d9cec59e96a5599b1cfe6fb4
Sadman007 <sakib.csedu21@gmail.com>
2021-05-05 00:21:19 +0600

    new files

---
+++ DevSkill_CP/Beginner/Batch_5/Class_01/code.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /// 1 <= n <= 10^8
+    int n;
+    cin >> n;
+    long long sum = (long long)(n) * (n + 1) / 2;
+    cout << sum << endl;
+    return 0;
+}
