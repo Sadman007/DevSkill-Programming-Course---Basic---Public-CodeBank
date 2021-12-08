b43664aef696667d9fc45a8cb61bf1a918435a86
Sadman007 <sakib.csedu21@gmail.com>
2021-12-09 00:19:34 +0600

    new files

---
+++ DevSkill_CP/Intermediate/Batch 11/class_22/code.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MAX 100000
+#define left st, (st + en) / 2, nd + nd
+#define right (st + en) / 2 + 1, en, nd + nd + 1
+
+int tree[4 * MAX + 5], arr[MAX + 5];
+void build(int st, int en, int nd)
+{
+    if (st == en)
+    {
+        tree[nd] = arr[st];
+        return;
+    }
+    build(left);
+    build(right);
+    tree[nd] = tree[nd + nd] + tree[nd + nd + 1];
+}
+int query(int st, int en, int nd, int L, int R)
+{
+    if (en < L || R < st) return 0;
+    if (L <= st && en <= R) return tree[nd];
+    return query(left, L, R) + query(right, L, R);
+}
+
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    build(0, n - 1, 1);
+    while(q--)
+    {
+        int l, r;
+        cin >> l >> r;
+        cout << query(0, n - 1, 1, l, r) << "\n";
+    }
+    return 0;
+}
