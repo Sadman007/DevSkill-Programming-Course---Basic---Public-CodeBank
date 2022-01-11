43f4245eb9f9ed4ffe2f93a0d809a694fe6a398a
Sadman007 <sakib.csedu21@gmail.com>
2022-01-12 01:49:14 +0600

    new files

---
+++ DevSkill_CP/DSA/batch_1/class_07/code.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /*
+    long long b, n = 10000000000; /// 10^8 = 0.25s
+    long long total_count = 0;
+    for (long long i = 0; i < n; i++)
+    {
+        b = (n * i) + 5;
+        total_count++;
+    }
+    cout << b << " " << total_count << "\n";
+    */
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j == k)
+            {
+                break;
+            }
+        }
+    }
+    return 0;
+}
