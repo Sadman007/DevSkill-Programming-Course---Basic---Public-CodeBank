91b8a1c3768579ea0c5a6904ea90dc192d70a784
Sadman007 <sakib.csedu21@gmail.com>
2022-01-17 23:56:12 +0600

    new files

---
+++ DevSkill_CP/DSA/batch_1/class_08/code.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "char: " << sizeof(char) << "\n";
+    cout << "int: " << sizeof(int) << "\n";
+    cout << "long long: " << sizeof(long long) << "\n";
+    cout << "float: " << sizeof(float) << "\n";
+    cout << "double: " << sizeof(double) << "\n";
+}
