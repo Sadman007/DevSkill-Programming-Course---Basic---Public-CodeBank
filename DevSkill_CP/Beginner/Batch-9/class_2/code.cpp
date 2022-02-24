28058aa3932a28dc1df2a553e7a5e66336947e0a
Sadman007 <sakib.csedu21@gmail.com>
2022-02-24 23:46:53 +0600

    new files

---
+++ DevSkill_CP/Beginner/Batch-9/class_2/code.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct StudentInfo
+{
+    string name;
+    int roll;
+    string section;
+};
+
+int main() /// main execution starts from here...
+{
+    /*
+    int c1 = 0;
+    short int c2 = 0;
+    long long int c3 = 0;
+    double c4 = 3.1416;
+    long double c5 = 3.34867357834;
+    char ch = 'f';
+    */
+
+    string username = "sakib"; // bikas
+    reverse(username.begin(), username.end());
+    cout << username << "\n";
+
+
+    /*
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(0);
+    vec.push_back(-1);
+    vec.push_back(2);
+    vec.push_back(-3);
+    sort(vec.begin(), vec.end());
+    for (int i = 0; i < vec.size(); i++)
+    {
+        cout << vec[i] << " ";
+    }
+    */
+    return 0;
+}
