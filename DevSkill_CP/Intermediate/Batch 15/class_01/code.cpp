0e8f40ade8a53f077ca1acefeb8f0e4f7363a1cb
Sadman007 <sakib.csedu21@gmail.com>
2023-05-12 01:47:14 +0800

    new files

---
+++ DevSkill_CP/Intermediate/Batch 15/class_01/code.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+    n = 100;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            sum++;
+        }
+    }
+    cout << sum << "\n";
+    return 0;
+}
